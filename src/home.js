fb545223bb165a2d7e9b10ba2c55f540d1b3154c
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2026-08-06 11:04:41 -0600

    Add home module with pseudo code

---
+++ src/home.js
@@ -0,0 +1,32 @@
+// IMPORT hero-img
+
+// CREATE section#home-hero
+  /**
+   * div#hero-container
+   *   h1 logo/text
+   *   img hero-img
+   */
+// CREATE section#home-info
+  /**
+   * div#home-info-container
+   *  div#home-hours-container
+   *    h2 hours
+   *    p info
+   *  div#home-gotomenu-container
+   *    p go to
+   *    button menu
+   *  div#home-findus-container
+   *    h2 find us
+   *    p info
+   *    div.socialmedia-icons-container
+   *      img socialmedia icon
+   *      img socialmedia icon
+   *      img socialmedia icon
+   */
+  // CREATE Footer#home-footer
+    /**
+     * p copyright info
+     */
+
+
+// EXPORT function
