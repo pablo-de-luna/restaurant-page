9346d27d8082d570ffb467f144026f562630ce7e
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2026-08-06 13:47:52 -0600

    Create and append hero and info section elements

--- src/home.js
+++ src/home.js
@@ -1,15 +1,15 @@
 import heroImg from "./assets/images/herobg.jpg";
 
-// CREATE section#home-hero
+// CREATE section hero
   /**
-   * div#hero-container
+   * section#hero-container
    *   h1 logo/text
    *   img hero-img
    *   p motto
    */
-// CREATE section#home-info
+// CREATE section info
   /**
-   * div#home-info-container
+   * section#home-info-container
    *  div#home-hours-container
    *    h2 hours
    *    p info
@@ -30,7 +30,48 @@ import heroImg from "./assets/images/herobg.jpg";
      */
 
 function createHomeElements() {
+  const content = document.querySelector("#content");
 
+  // Hero section 
+  const heroSection = document.createElement("section");  
+  heroSection.id = "hero-section";
+
+    const heroLogo = document.createElement("h1"); 
+    heroLogo.textContent = "PIZZATOP"
+    
+    const heroMotto = document.createElement("p");
+    heroMotto.textContent = "THE BEST PIZZA IN THE WORLD!"
+
+    const heroBg = document.createElement("img");
+    heroBg.src = heroImg;
+
+  heroSection.appendChild(heroLogo);
+  heroSection.appendChild(heroMotto);
+  heroSection.appendChild(heroBg);
+
+  // Info section
+  const infoSection = document.createElement("section");
+  infoSection.id = "info-section";
+
+  const hours = document.createElement("div");
+  hours.id = "home-hours-container";
+
+    const hoursTitle = document.createElement("h2");
+    hoursTitle.textContent = "HOURS";
+
+    const hoursText = document.createElement("p");
+    const hoursTextContent = `Mon-Wed: 11:00 AM - 10:00 PM<br>
+                      Thu-Fri: 11:00 AM - 12:AM<br>
+                      Sat: 8:00 AM - 4:00 PM`;
+    hoursText.innerHTML = hoursTextContent;
+
+    hours.appendChild(hoursTitle);
+    hours.appendChild(hoursText);
+
+  infoSection.appendChild(hours);
+
+  content.appendChild(heroSection);
+  content.appendChild(infoSection);
 }
 
 export default createHomeElements;
