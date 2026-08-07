cdaaf1f38a839e80dccfcd601d5606b93347cc1f
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2026-08-07 14:11:50 -0600

    Create function to create and append menu elements
    Create AddMenuElements function to create and append menu elements
    Create helper function to dynamically generate the pizza item containers
    and its nested elements using a loop

--- src/pages-modules/menu.js
+++ src/pages-modules/menu.js
@@ -1,47 +1,65 @@
-import margherita from "../assets/images/margherita.png";
-import pepperoni from "../assets/images/pepperoni.png";
 import prosciutto from "../assets/images/prosciutto.png";
+import pepperoni from "../assets/images/pepperoni.png";
+import margherita from "../assets/images/margherita.png";
 
-const pizzasInfo = [
+const pizzas = [
   {
+    image: prosciutto,
     name:"Prosciutto & Arugula",
     description:"Garlic olive oil base topped with melted mozzarella, crispy prosciutto, fresh arugula, and a drizzle of white truffle oil."
   },
   {
+    image: pepperoni,
     name:"Double Pepperoni Craze",
     description:"Rich tomato sauce, mozzarella, loaded with crispy cupped pepperoni, finished with fresh basil and shredded parmesan."
   },  
   {
+    image: margherita,
     name:"Fresh Basil Margherita",
     description:"House tomato sauce, fresh mozzarella, sweet cherry tomatoes, aromatic basil leaves, and a sprinkle of aged parmesan."
   }
 ];
 
-export default function AddMenuElements() {
-// CREATE and APPEND elements
-/**
-  div#menu-title
-  div#menu-container
-    div.menu-item-container
-      img
-      div.item-text
-        h1 
-        p
-    div.menu-item-container
-      img
-      div.item-text
-        h1 
-        p
-    div.menu-item-container
-      img
-      div.item-text
-        h1 
-        p
- */
+const content = document.querySelector("#content");
+
+function createAndAppendPizzasContainerElements(menuContainer) {
+  for (let i = 0; i < pizzas.length; i++) {
+    const pizzaContainer = document.createElement("div");
+    pizzaContainer.className = "pizza-container";
+
+    const pizzaImage = document.createElement("img");
+    pizzaImage.src = pizzas[i].image;
+    pizzaImage.setAttribute("alt", "pizza image");
+
+    const pizzaTextContainer = document.createElement("div");
+    pizzaTextContainer.className = "pizza-text";
+
+    const pizzaName = document.createElement("h1");
+    pizzaName.textContent = pizzas[i].name;
+
+    const pizzaDescription = document.createElement("p");
+    pizzaDescription.textContent = pizzas[i].description;
+
+    pizzaTextContainer.appendChild(pizzaName);
+    pizzaTextContainer.appendChild(pizzaDescription);
+    pizzaContainer.appendChild(pizzaImage);
+    pizzaContainer.appendChild(pizzaTextContainer);
+
+    menuContainer.appendChild(pizzaContainer);
+  }
+}
+
+function AddMenuElements() {
   const menuTitle = document.createElement("div");
   menuTitle.id = "menu-title";
+  menuTitle.textContent = "MENU";
 
   const menuContainer = document.createElement("div");
   menuContainer.id = "menu-container";
 
+  content.appendChild(menuTitle);
+  createAndAppendPizzasContainerElements(menuContainer);
+  content.appendChild(menuContainer);
 }
+
+export default AddMenuElements;
