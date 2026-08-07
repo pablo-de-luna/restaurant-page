d34e4d0ee484b86afafe09578b941e7ad7c65745
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2026-08-07 12:49:26 -0600

    Create array of objects with each pizza info

--- src/pages-modules/menu.js
+++ src/pages-modules/menu.js
@@ -2,16 +2,20 @@ import margherita from "../assets/images/margherita.png";
 import pepperoni from "../assets/images/pepperoni.png";
 import prosciutto from "../assets/images/prosciutto.png";
 
-/** PIZZA INFO
-  Prosciutto & Arugula
-  Garlic olive oil base topped with melted mozzarella, crispy prosciutto, fresh arugula, and a drizzle of white truffle oil.
-
-  Double Pepperoni Craze
-  Rich tomato sauce, mozzarella, loaded with crispy cupped pepperoni, finished with fresh basil and shredded parmesan.
-
-  Fresh Basil Margherita
-  House tomato sauce, fresh mozzarella, sweet cherry tomatoes, aromatic basil leaves, and a sprinkle of aged parmesan.
- */
+const pizzasInfo = [
+  {
+    name:"Prosciutto & Arugula",
+    description:"Garlic olive oil base topped with melted mozzarella, crispy prosciutto, fresh arugula, and a drizzle of white truffle oil."
+  },
+  {
+    name:"Double Pepperoni Craze",
+    description:"Rich tomato sauce, mozzarella, loaded with crispy cupped pepperoni, finished with fresh basil and shredded parmesan."
+  },  
+  {
+    name:"Fresh Basil Margherita",
+    description:"House tomato sauce, fresh mozzarella, sweet cherry tomatoes, aromatic basil leaves, and a sprinkle of aged parmesan."
+  }
+];
 
 export default function AddMenuElements() {
 // CREATE and APPEND elements
@@ -34,5 +38,10 @@ export default function AddMenuElements() {
         h1 
         p
  */
+  const menuTitle = document.createElement("div");
+  menuTitle.id = "menu-title";
+
+  const menuContainer = document.createElement("div");
+  menuContainer.id = "menu-container";
 
 }
